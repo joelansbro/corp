6fed98d720682ac3da343c3d914aff519dc9baa4
Joel Ansbro <joelmatthewansbro@gmail.com>
2021-09-28 23:11:32 +0100

    initial

---
+++ readme.md
@@ -0,0 +1,8 @@
+## Corp
+
+A mockup of Bloomberg via Tailwind CSS.
+
+### Commands
+When making changes to src/style.css, save and then run:
+<code>npm run build:css</code>
+to enforce changes into the Tailwind styles
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      container: {
+          padding: '2rem',
+      }
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
